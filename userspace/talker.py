36b684dc89125947045aa82bc52ef76a7c34085b
Tom Jones <jones@sdf.org>
2017-07-25 16:18:02 +0100

    Add example listener and talker

---
+++ userspace/talker.py
@@ -0,0 +1,12 @@
+from scapy.all import IP
+from scapy.all import UDP 
+from scapy.all import *
+
+import udpoptions
+import udp_usrreq
+
+if __name__ == "__main__":
+    opts = { 'UDPOPT_TIME': (0x11223344, 0x55667788), 'UDPOPT_MSS': 0x1122}
+    udp_usrreq.udp_output("Hello Options Space on a packet\n", 
+        {"src":"139.133.204.4", "dst":"139.133.204.54", "sport":2500, "dport":2600}, 
+        options=opts)
