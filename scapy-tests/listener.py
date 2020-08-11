ea6acab651acc618dbd35d1617c648d4f40cd088
Your Name <you@example.com>
2020-08-11 09:23:24 +0000

    Tidy up and make more things vary

--- scapy-tests/listener.py
+++ scapy-tests/listener.py
@@ -1,6 +1,10 @@
 import udp_options
 import udp_usrreq
 
+BINDADDR = '10.0.4.1'
+SPORT = 7
+INTERFACE = None
+
 def callback(pcb, data=None, options=None, error=None):
     if error:
         print("error from:")
@@ -18,14 +22,14 @@ def callback(pcb, data=None, options=None, error=None):
     # be an echo server
     if data and not error:
         udp_usrreq.udp_output(data, 
-            {"src": pcb['address'], "dst": pcb['peerinfo'][0], "sport": 2600, "dport": 2500})
+            {"src": pcb['address'], "dst": pcb['peerinfo'][0], "sport": SPORT, "dport": 2500})
 
 if __name__ == "__main__":
     print("startings")
     pcb_hdr = udp_usrreq.bindaddr(
         {
-            "address": '10.0.4.4', 
-            "port": 2600, 
+            "address": BINDADDR,
+            "port": SPORT,
             "cb": callback
         })
     if not pcb_hdr:
@@ -33,4 +37,4 @@ if __name__ == "__main__":
         exit(1)
     else:
         print("bound and listening on ", pcb_hdr)
-    udp_usrreq.run_loop()
+    udp_usrreq.run_loop(INTERFACE)
